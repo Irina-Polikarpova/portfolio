cf7a923ca6660a7c47b8d76ec9f046414482b7c6
Irina-Polikarpova <82443751+Irina-Polikarpova@users.noreply.github.com>
2021-04-13 13:45:14 +0300

    Add files via upload

---
+++ js/theme.js
@@ -0,0 +1,94 @@
+(function($) {
+	'use strict';
+
+	var nav_offset_top = $('header').height() + 50;
+	/*-------------------------------------------------------------------------------
+	  Navbar 
+	-------------------------------------------------------------------------------*/
+
+	//* Navbar Fixed
+	function navbarFixed() {
+		if ($('.header_area').length) {
+			$(window).scroll(function() {
+				var scroll = $(window).scrollTop();
+				if (scroll >= nav_offset_top) {
+					$('.header_area').addClass('navbar_fixed');
+				} else {
+					$('.header_area').removeClass('navbar_fixed');
+				}
+			});
+		}
+	}
+	navbarFixed();
+
+	/*----------------------------------------------------*/
+	/*  MailChimp Slider
+    /*----------------------------------------------------*/
+	function mailChimp() {
+		$('#mc_embed_signup').find('form').ajaxChimp();
+	}
+	mailChimp();
+
+	$('select').niceSelect();
+	/* ---------------------------------------------
+            Isotope js Starts
+         --------------------------------------------- */
+	$(window).on('load', function() {
+		$('.portfolio-filter ul li').on('click', function() {
+			$('.portfolio-filter ul li').removeClass('active');
+			$(this).addClass('active');
+
+			var data = $(this).attr('data-filter');
+			$workGrid.isotope({
+				filter: data
+			});
+		});
+
+		if (document.getElementById('portfolio')) {
+			var $workGrid = $('.portfolio-grid').isotope({
+				itemSelector: '.all',
+				percentPosition: true,
+				masonry: {
+					columnWidth: '.all'
+				}
+			});
+		}
+	});
+
+	/*----------------------------------------------------*/
+	/* Start Magnific Pop Up
+	/*----------------------------------------------------*/
+	if ($('.img-gal').length > 0) {
+		$('.img-gal').magnificPopup({
+			type: 'image',
+			gallery: {
+				enabled: true
+			}
+		});
+	}
+	/*----------------------------------------------------*/
+	/*  End  Magnific Pop Up
+	/*----------------------------------------------------*/
+
+	/*----------------------------------------------------*/
+	/*  Testimonials Slider
+    /*----------------------------------------------------*/
+	function testimonials_slider() {
+		if ($('.owl-carousel').length) {
+			$('.owl-carousel').owlCarousel({
+				loop: true,
+				items: 1,
+				autoplay: false,
+				nav: true,
+				navText:["<div class='nav-btn prev-slide'></div>","<div class='nav-btn next-slide'></div>"],
+			});
+		}
+	}
+	testimonials_slider();
+
+	if (window.location.href.substr(-9) === "font.html") {
+		$('.prev-slide').css({left: '-15%'});
+		$('.next-slide').css({left: '110%'});
+	}
+
+})(jQuery);
